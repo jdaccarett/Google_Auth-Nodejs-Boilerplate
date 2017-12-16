680fb8d78ad4228618a8298caf42e53dc41ee936
jdaccarett <jdaccarett101@gmail.com>
2017-12-16 15:31:36 -0800

    updated config

--- config/keys.js
+++ config/keys.js
@@ -1,7 +1,8 @@
-if (process.env.NODE_ENV === "production") {
-  // we are in production - return the prod set of keys
-  module.exports = require("./prod");
-} else {
-  // we are in development - return the dev keys!!!
-  module.exports = require("./dev");
-}
+module.exports = {
+  googleClientID:
+    "595031700510-kb5mv11shb3b3rd9c0l5ganubg354rh8.apps.googleusercontent.com",
+  googleClientSecret: "STIEeDDbVWwT3Tp4g0owLjB6",
+  mongoURI:
+    "mongodb://jdaccarett:weeman91@ds159776.mlab.com:59776/survey_analytics-dev",
+  cookieKey: "lsjflsdjflsdjflsdhfkghggl3jlajdlnhg"
+};
