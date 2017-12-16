d6c8a2737d897fb92777892f46e9c6f00569faab
jdaccarett <jdaccarett101@gmail.com>
2017-12-16 11:53:10 -0800

    Google Authentication

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const cookiesSession = require("cookie-session");
+const passport = require("passport");
+const keys = require("./config/keys");
+require("./models/User");
+require("./services/passport");
+
+mongoose.connect(keys.mongoURI);
+
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send({ hi: "there" });
-});
+// cookie setup
+app.use(
+  cookiesSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000, // 30days
+    keys: [keys.cookieKey]
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+require("./routes/authRoutes")(app);
 
 // If there isnt environment variables by heroku use 5000;
 const PORT = process.env.PORT || 5000;
