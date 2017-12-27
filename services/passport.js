c0d6dabc35d0c4f594c2a50efc1bd7200bcbb000
jdaccarett <jdaccarett101@gmail.com>
2017-12-27 10:28:10 -0800

    added React client side

--- services/passport.js
+++ services/passport.js
@@ -23,19 +23,16 @@ passport.use(
       callbackURL: "/auth/google/callback",
       proxy: true
     },
-    (accessToken, refreshToken, profile, done) => {
-      User.findOne({ googleId: profile.id }).then(existingUser => {
-        if (existingUser) {
-          // we already have a record with given profile id.
-          console.log("Profile Already Exists");
-          done(null, existingUser);
-        } else {
-          // we dont have a use record with this id, make a new record.
-          new User({ googleId: profile.id })
-            .save()
-            .then(user => done(null, user));
-        }
-      });
+    async (accessToken, refreshToken, profile, done) => {
+      const existingUser = await User.findOne({ googleId: profile.id });
+      if (existingUser) {
+        // we already have a record with given profile id.
+        console.log("Profile Already Exists");
+        return done(null, existingUser);
+      }
+      // we dont have a use record with this id, make a new record.
+      const user = await new User({ googleId: profile.id }).save();
+      done(null, user);
     }
   )
 );
